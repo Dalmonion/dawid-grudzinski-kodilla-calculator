448b8680eb7765cc8f3f434b2633256f14760c5a
Dawid Grudziński <ddgrudzinski@gmail.com>
2020-11-25 14:44:08 +0100

    First Commit

---
+++ src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+
+
+}
